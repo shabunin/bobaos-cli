2a3db9cc673932df6824d177b32a08059b173499
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-01-05 23:19:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# bobaos-cli
+
+CLI tool to commicate with KNX BAOS 838 module kBerry.
+
+# Installation
+
+1. Prepare your Raspberry Pi: install raspbian, enable ssh. Or you could download my image [here](https://drive.google.com/file/d/14nKNbaQfCUN9Mu7cFc5JTicbgbWo06kt/view?usp=sharing). In this case you should go directly to step 4. Image is based on 2017-11-29-raspbian-stretch-lite with installed nodejs 8, vim, git, enabled ssh and correct config.txt, cmdline.txt.
+
+2. Install [KNX BAOS Module 838 kBerry](https://www.weinzierl.de/index.php/en/all-knx/knx-module-en/knx-baos-module-838-en) shield to your Raspberry Pi.
+
+3 [Set up serial port](https://github.com/weinzierl-engineering/baos/blob/master/docs/Raspbian.adoc#kberry)
+
+4. Install nodejs, git
+```sh
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs git
+```
+5. Install this package globally:
+```sh
+sudo npm install -g bobaos-cli
+```
+
+# Usage
+```sh
+bobaos-cli
+```
+Now we are in interactive shell. First of all, get help:
+
+```
+help
+```
+
+```
+Commands:
+
+help [command...]                   Provides help for a given command.
+exit                                Exits application.
+open [options]                      Open serial port
+getDatapointDescription [options]   GetDatapointDescription.Req service
+setDatapointValue [options]         SetDatapointValue.Req service with command "set and send to bus"
+readDatapointFromBus [options]      SetDatapointValue.Req service with command "read via bus"
+getDatapointValue [options]         GetDatapointValue.Req service
+getParameterByte [options]          GetParameterByte.Req service
+```
+
+Open port and then send any command you want. Example:
+
+```
+bobaos> open
+bobaos> getDatapoint
+getDatapointDescription  getDatapointValue
+bobaos> getDatapointDescription -s 1 -n 10
+bobaos> { service: 'GetDatapointDescription.Res',
+  direction: 'response',
+  error: false,
+  start: 1,
+  number: 10,
+  payload:
+    [ { id: 1, valueType: 8, configFlags: 95, dpt: 'dpt9'  },
+  { id: 2, valueType: 7, configFlags: 87, dpt: 'dpt5'  },
+  { id: 3, valueType: 7, configFlags: 87, dpt: 'dpt5'  },
+  { id: 4, valueType: 7, configFlags: 87, dpt: 'dpt5'  },
+  { id: 5, valueType: 7, configFlags: 87, dpt: 'dpt5'  },
+  { id: 6, valueType: 0, configFlags: 95, dpt: 'dpt1'  },
+  { id: 7, valueType: 0, configFlags: 95, dpt: 'dpt1'  },
+  { id: 8, valueType: 0, configFlags: 87, dpt: 'dpt1'  },
+  { id: 9, valueType: 0, configFlags: 87, dpt: 'dpt1'  },
+  { id: 10, valueType: 7, configFlags: 83, dpt: 'dpt5'  } ] }
+bobaos>
+bobaos> getDatapointValue -s 1 -n 10
+bobaos> { service: 'GetDatapointValue.Res',
+    direction: 'response',
+    error: false,
+    start: 1,
+    number: 10,
+    payload:
+      [ { id: 1, state: 16, length: 2, value: <Buffer 0e 36>  },
+    { id: 2, state: 16, length: 1, value: <Buffer 00>  },
+    { id: 3, state: 16, length: 1, value: <Buffer 00>  },
+    { id: 4, state: 0, length: 1, value: <Buffer 00>  },
+    { id: 5, state: 16, length: 1, value: <Buffer 00>  },
+    { id: 6, state: 0, length: 1, value: <Buffer 00>  },
+    { id: 7, state: 0, length: 1, value: <Buffer 00>  },
+    { id: 8, state: 0, length: 1, value: <Buffer 00>  },
+    { id: 9, state: 0, length: 1, value: <Buffer 00>  },
+    { id: 10, state: 0, length: 1, value: <Buffer 00>  } ] }
+bobaos>
+```
+
+Thats it.
---
+++ index.js
@@ -0,0 +1,173 @@
+const Baos = require('bobaos');
+const Vorpal = require('vorpal');
+const DPTS = require('knx-dpts-baos');
+
+//app.on('service', console.log);
+let app = null;
+
+const vorpal = Vorpal();
+vorpal
+  .command('open', 'Open serial port')
+  .option('-d, --debug', 'Print debug information on FT12 communication')
+  .option('-p, --port <str>', 'Optional. Default is "/dev/ttyAMA0". Serialport device.')
+  .option('-i, --ind', 'Optional. Log *.Ind service')
+  .action(function (args, callback) {
+    try {
+      let debug = false;
+      let device = '/dev/ttyAMA0';
+      let indication = false;
+      if (typeof args.options.debug !== 'undefined') {
+        debug = args.options.debug;
+      }
+      if (typeof args.options.port !== 'undefined') {
+        device = args.options.port;
+      }
+      if (typeof args.options.ind !== 'undefined') {
+        indication = args.options.ind
+      }
+
+      let params = {
+        debug: debug,
+        serialPort: {device: device}
+      };
+      // create bobaos instance
+      app = new Baos(params);
+      // register callback
+      app.on('service', function (data) {
+        if (indication) {
+          console.log(data)
+        } else {
+          if (data.direction !== 'indication') {
+            console.log(data)
+          }
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      callback();
+    }
+  });
+
+vorpal
+  .command('getDatapointDescription', 'GetDatapointDescription.Req service')
+  .option('-s, --start <number>', 'Required. Id of first datapoint')
+  .option('-n, --number <number>', 'Optional. Default = 1')
+  .action(function (args, callback) {
+    try {
+      let number = 1;
+      if (typeof args.options.start === 'undefined') {
+        throw new Error('Please specify datapoint start');
+      }
+      if (typeof args.options.number !== 'undefined') {
+        number = args.options.number;
+      }
+      let start = args.options.start;
+      app.getDatapointDescription(start, number);
+    } catch (e) {
+      console.log(e)
+    } finally {
+      callback();
+    }
+  });
+
+vorpal
+  .command('setDatapointValue', 'SetDatapointValue.Req service with command "set and send to bus"')
+  .option('-s, --start <number>', 'Required. Id of datapoint')
+  .option('-v, --value <str>', 'Required.')
+  .option('-t, --type <str>', 'Required. DPT type of datapoint: dpt1-dpt18')
+  .action(function (args, callback) {
+    try {
+      if (typeof args.options.start === 'undefined') {
+        throw new Error('Please specify datapoint start');
+      }
+      if (typeof args.options.value === 'undefined') {
+        throw new Error('Please specify datapoint value');
+      }
+      if (typeof args.options.type === 'undefined') {
+        throw new Error('Please specify datapoint type');
+      }
+      let start = args.options.start;
+      let value = args.options.value;
+      let type = args.options.type;
+      if (!Object.prototype.hasOwnProperty.call(DPTS, type)) {
+        throw new TypeError('Please specify correct datapoint type')
+      }
+      let valueBuff = DPTS[type].fromJS(value);
+      app.setDatapointValue(start, valueBuff);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      callback();
+    }
+  });
+
+vorpal
+  .command('readDatapointFromBus', 'SetDatapointValue.Req service with command "read via bus"')
+  .option('-s, --start <number>', 'Required. Id of first datapoint')
+  .option('-l, --length <number>', 'Required. Length of datapoint value')
+  .action(function (args, callback) {
+    try {
+      if (typeof args.options.start === 'undefined') {
+        throw new Error('Please specify datapoint start');
+      }
+      if (typeof args.options.length === 'undefined') {
+        throw new Error('Please specify datapoint start');
+      }
+      let start = args.options.start;
+      let length = args.options.length;
+      app.readDatapointFromBus(start, length);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      callback();
+    }
+  });
+
+vorpal
+  .command('getDatapointValue', 'GetDatapointValue.Req service')
+  .option('-s, --start <number>', 'Required. Id of first datapoint')
+  .option('-n, --number <number>', 'Optional. Default = 1')
+  .action(function (args, callback) {
+    try {
+      let number = 1;
+      if (typeof args.options.start === 'undefined') {
+        throw new Error('Please specify datapoint start');
+      }
+      if (typeof args.options.number !== 'undefined') {
+        number = args.options.number;
+      }
+      let start = args.options.start;
+      app.getDatapointValue(start, number);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      callback();
+    }
+  });
+
+vorpal
+  .command('getParameterByte', 'GetParameterByte.Req service')
+  .option('-s, --start <number>', 'Required. Id of first datapoint')
+  .option('-n, --number <number>', 'Optional. Default = 1')
+  .action(function (args, callback) {
+    try {
+      let number = 1;
+      if (typeof args.options.start === 'undefined') {
+        throw new Error('Please specify datapoint start');
+      }
+      if (typeof args.options.number !== 'undefined') {
+        number = args.options.number;
+      }
+      let start = args.options.start;
+      app.getParameterByte(start, number);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      callback();
+    }
+  });
+
+vorpal
+  .delimiter('bobaos>')
+  .show();
